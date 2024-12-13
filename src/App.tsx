0fa91d4a8be78de6acd1dfd8df7657ea7cd9afca
Bartosz <bartosz.burdalski@gmail.com>
2024-12-13 05:11:53 +0100

    Add emotion js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import "./App.css";
 import { useStore } from "../store";
+import styled from "@emotion/styled";
+
+const StyledH1 = styled.h1(() => ({
+  fontSize: "100px",
+}));
+
+const StyledH2 = StyledH1.withComponent("h2");
+
+const Component = ({ className }: { className?: string }) => {
+  return <div className={className}>Component</div>;
+};
+
+const StyledComponent = styled(Component)(() => ({
+  color: "red",
+}));
 
 function App() {
   const str = useStore((state) => state.str);
-  return <h1>{str}</h1>;
+  return (
+    <>
+      <StyledH1>{str}</StyledH1>
+      <StyledComponent />
+      <StyledH2>H2</StyledH2>
+    </>
+  );
 }
 
 export default App;
