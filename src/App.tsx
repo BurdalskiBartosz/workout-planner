22d4813313f448d262f0156a51fbab6ae7f668fa
Bartosz <bartosz.burdalski@gmail.com>
2024-12-13 05:51:33 +0100

    Add babel plugin to vite config and update slightly app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import "./App.css";
 import { useStore } from "../store";
 import styled from "@emotion/styled";
 
-const StyledH1 = styled.h1(() => ({
-  fontSize: "100px",
-}));
+const Child = styled.p({
+  fontSize: "15px",
+  color: "yellow",
+});
+
+const StyledH1 = styled.h1<{ fontSize: number }>(
+  {
+    fontSize: "100px",
+    [`& ${Child}`]: {
+      color: "blue",
+    },
+  },
+  (props) => ({
+    fontSize: props.fontSize,
+  })
+);
 
 const StyledH2 = StyledH1.withComponent("h2");
 
@@ -20,9 +33,14 @@ function App() {
   const str = useStore((state) => state.str);
   return (
     <>
-      <StyledH1>{str}</StyledH1>
+      <StyledH1 fontSize={120}>
+        {str}
+        <Child>BLUE</Child>
+        <p>XD</p>
+      </StyledH1>
+      <Child>YELLOW</Child>
       <StyledComponent />
-      <StyledH2>H2</StyledH2>
+      <StyledH2 fontSize={80}>H2</StyledH2>
     </>
   );
 }
