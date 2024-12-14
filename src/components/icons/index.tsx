283b90c2f015bd945606119932284b1c8a57b834
Bartosz <bartosz.burdalski@gmail.com>
2024-12-15 00:44:54 +0100

    Add icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as Calendar } from "./Calendar";
+export { default as Category } from "./Category";
+export { default as PieChart } from "./PieChart";
+export { default as Notification } from "./Notification";
+export { default as Settings } from "./Settings";
