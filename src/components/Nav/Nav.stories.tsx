87172b1d8725e5af1076cf34fa8995e24e87a32a
Bartosz <bartosz.burdalski@gmail.com>
2024-12-17 23:52:19 +0100

    Add Nav story

---
+++ src/components/Nav/Nav.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Nav } from "./index";
+import { BrowserRouter } from "react-router";
+
+const meta = {
+  title: "Example/Nav",
+  parameters: {
+    layout: "centered",
+  },
+  component: Nav,
+} satisfies Meta<typeof Nav>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
