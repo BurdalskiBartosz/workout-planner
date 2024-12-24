2ced63bde615a1cc91c1d80d710d64bcf5c55b8c
Bartosz <bartosz.burdalski@gmail.com>
2024-12-24 03:34:11 +0100

    Refactor AppTemplate styles into separate file for better organization

---
+++ src/templates/AppTemplate.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const StyledWraper = styled.div`
+  display: grid;
+  grid-template-columns: 250px 1fr;
+  height: 100vh;
+`;
+
+export const StyledMain = styled.main`
+  padding: 60px 40px 40px;
+`;
